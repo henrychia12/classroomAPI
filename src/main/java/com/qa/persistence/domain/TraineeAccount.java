2c22240d370ddb534cdb80349118b1e2a1338d42
henrychia12 <henry_chia21@hotmail.co.uk>
2019-01-18 15:30:14 +0000

    Table notation on persistence.domain (account & traineeAccount)

--- src/main/java/com/qa/persistence/domain/TraineeAccount.java
+++ src/main/java/com/qa/persistence/domain/TraineeAccount.java
@@ -1,9 +1,13 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Trainee")
 public class TraineeAccount {
 	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
